0bcb79b3ab019252620a197f040ed59f8643aea3
Ryan Dulac <analyticrye@gmail.com>
2018-10-31 00:30:26 -0400

    changed layout

--- functions.php
+++ functions.php
@@ -7,7 +7,6 @@ function DNFiles() {
     wp_enqueue_script('jquery', get_template_directory_uri() .'/js/jquery-3.3.1.slim.min.js', array('jquery'), null, true);
 	wp_enqueue_script( 'bootstrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js', array( 'jquery' ), "4.1.3", true);
     wp_enqueue_style('bootstrap-css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css');
-    
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     //null for no depedencies and microtime() to avoid caching. Remove before deploying
     wp_enqueue_style('DNMainStyles', get_stylesheet_uri(), null, microtime());

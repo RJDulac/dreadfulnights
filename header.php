2215fa8fd61386c3e6f9eececd15394cf7924f43
Matt Duong <matt.duong@gmail.com>
2018-10-31 00:35:41 -0400

    Nav and Logo Update

--- header.php
+++ header.php
@@ -5,14 +5,12 @@
 </head>
 <body>
 
-        <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
             <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-                <a class="navbar-brand" href="#">Dark Nights</a>
-
-            <?php wp_nav_menu(); ?>
+                <a class="navbar-brand" href="#"><img src="/wp-content/themes/dreadful-theme/img/dreadful-logo.svg" alt="Dreadful Nights logo"/></a>
 
                 <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
                     <li class="nav-item active">

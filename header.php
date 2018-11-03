9fb36fd361a1b7a6f01a6562fbe4dee8e4d8f420
Ryan Dulac <analyticrye@gmail.com>
2018-11-02 22:01:38 -0400

    test

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
                         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
+                        <a class="nav-link" href="#">Test</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link disabled" href="#">Disabled</a>

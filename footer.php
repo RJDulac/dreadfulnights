65e71384687366f6d7358ffaffc71e64060b0462
Ryan Dulac <analyticrye@gmail.com>
2018-11-02 21:30:51 -0400

    added dynamic wp function for image links

---
+++ footer.php
@@ -0,0 +1,4 @@
+<p class="text-center">Copyright Dreadful Nights</p>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ single.php
@@ -0,0 +1,18 @@
+<!-- wordpress looks for this file for single posts -->
+<?php
+get_header();
+// wordpress loop
+?>
+<div class="container">
+<?php
+while(have_posts()) {
+    //wordpress function that keeps track of posts
+    the_post(); ?>
+    <h2><?php the_title();?><h2>
+    <?php the_content(); ?>
+<?php }
+?>
+</div>
+<?php
+get_footer();
+?>

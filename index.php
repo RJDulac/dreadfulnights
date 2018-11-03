0d45306184a14e2ea5d9fa463cb19212084e70cb
Ryan Dulac <analyticrye@gmail.com>
2018-11-02 21:35:50 -0400

    added dynamic wp function for image links

--- index.php
+++ index.php
@@ -19,7 +19,6 @@
 <div class="col-md-8">
 <?php
 while(have_posts()) {
-    //wordpress function that keeps track of posts
     the_post(); ?>
     <h2><a href="<?php the_permalink();?>"><?php the_title();?></a><h2>
     <?php the_excerpt(); ?>
